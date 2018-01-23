10f9e9fceffa4fbf4a37e6da9494cab232ae9bd7
Kyurator <35382347+kyurator@users.noreply.github.com>
2018-01-23 01:08:51 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-// alert('scripts are running');
+function init() {
+ Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/e/2PACX-1vSfeFW2F060OPu6-nKkd11zr7_nAxnZj1sIhn0d6CFWoG_D1P0R9DzMhIkD_pnZDiRyYJ_p11MhlY6F/pubhtml',
+ callback: function(data, tabletop) { 
+ console.log(data)
+ },
+ simpleSheet: true } )
+}
+window.addEventListener(‘DOMContentLoaded’, init)
+
