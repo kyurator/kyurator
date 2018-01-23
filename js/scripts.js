f775f1ec072b633940c5d4df23d96dbfd470b9ef
Kyurator <35382347+kyurator@users.noreply.github.com>
2018-01-23 01:46:04 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function init() {
- Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/e/2PACX-1vSfeFW2F060OPu6-nKkd11zr7_nAxnZj1sIhn0d6CFWoG_D1P0R9DzMhIkD_pnZDiRyYJ_p11MhlY6F/pubhtml',
+ Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/10yoc-VUUL50KmM7iq7VJNgbOYFnJXpgVddbpgsYflq0/pubhtml',
  callback: function(data, tabletop) { 
  console.log(data)
  },
